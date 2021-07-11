50e7e5212a9cc7c03b99bd5d87160d56ada35646
Tina <suporte@geradordeideias.com>
2021-07-11 16:15:39 -0300

    Projeto Gama Academy - JavaScript II

---
+++ database.js
@@ -0,0 +1,28 @@
+const livros = [
+    {
+        id: 1,
+        nome: "O dia do Curinga",
+        author: "Jostein Gaarder",
+        categoria: "Romance",
+        paginas: 351,
+        leu: false
+    },
+    {
+        id: 2,
+        nome: "O retrato Dorian Grey",
+        author: "Oscar Wilde",
+        categoria: "Psicológico",
+        paginas: 267,
+        leu: false
+    },
+    {
+        id: 3,
+        nome: "Diário de Anne Frank",
+        author: "Anne Frank",
+        categoria: "Romance",
+        paginas: 276,
+        leu: false
+    }
+]
+
+module.exports = livros
